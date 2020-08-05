49d2f954bc423992fe899049f1d203b2acc6cc60
alex <alexshtanko@gmail.com>
2020-08-05 10:47:16 +0300

    add header filter template

---
+++ template-parts/content-header-filter.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package konferencii2
+ */
+
+?>
+
+<div class="header-filter">
+    <div class="filter-item">
+        <div class="header-filter-title subject">Тематика</div>
+
+        <div class="header-filter-body">
+            <div class="scrollbar-inner">
+                <div class="scrollbar-inner-content">
+                    <ul class="subject-list">
+                        <li>
+                            <input id="1" type="checkbox" name="">
+                            <label for="1">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="2" type="checkbox" name="">
+                            <label for="2">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="3" type="checkbox" name="">
+                            <label for="3">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="4" type="checkbox" name="">
+                            <label for="4">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="5" type="checkbox" name="">
+                            <label for="5">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="6" type="checkbox" name="">
+                            <label for="6">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="7" type="checkbox" name="">
+                            <label for="7">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="8" type="checkbox" name="">
+                            <label for="8">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="9" type="checkbox" name="">
+                            <label for="9">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="10" type="checkbox" name="">
+                            <label for="10">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="11" type="checkbox" name="">
+                            <label for="11">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="12" type="checkbox" name="">
+                            <label for="12">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="13" type="checkbox" name="">
+                            <label for="13">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="14" type="checkbox" name="">
+                            <label for="14">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="15" type="checkbox" name="">
+                            <label for="15">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="16" type="checkbox" name="">
+                            <label for="16">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="17" type="checkbox" name="">
+                            <label for="17">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="18" type="checkbox" name="">
+                            <label for="18">Безопасность</label>
+                        </li>
+                        <li>
+                            <input id="19" type="checkbox" name="">
+                            <label for="19">Безопасность</label>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="header-filter-body-footer">
+                <button class="btn-filter-submit">Применить</button>
+            </div>
+        </div><!--header-filter-body end here-->
+    </div>
+
+    <div class="filter-item">
+        <div class="header-filter-title date">Дата</div>
+
+        <div class="header-filter-body">
+            <!-- <div class="scrollbar-inner"> -->
+                <div class="scrollbar-inner-content">
+                    Text
+                </div>
+            <!-- </div> -->
+
+            <div class="header-filter-body-footer">
+                <button class="btn-filter-submit">Применить</button>
+            </div>
+        </div><!--header-filter-body end here-->
+    </div>
+
+    <div class="filter-item">
+        <div class="header-filter-title location">Место проведения</div>
+
+        <div class="header-filter-body">
+            <!-- <div class="scrollbar-inner"> -->
+                <div class="scrollbar-inner-content">
+                    Text
+                </div>
+            <!-- </div> -->
+
+            <div class="header-filter-body-footer">
+                <button class="btn-filter-submit">Применить</button>
+            </div>
+        </div><!--header-filter-body end here-->
+    </div>
+
+    <div class="filter-item">
+        <div class="header-filter-title other">Другие параметры</div>
+
+        <div class="header-filter-body">
+            <!-- <div class="scrollbar-inner"> -->
+                <div class="scrollbar-inner-content">
+                    Text
+                </div>
+            <!-- </div> -->
+
+            <div class="header-filter-body-footer">
+                <button class="btn-filter-submit">Применить</button>
+            </div>
+        </div><!--header-filter-body end here-->
+    </div>
+</div><!--header-filter end here-->
