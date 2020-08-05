ad54abb6bff0649bf18faddff46503e18517f615
alex <alexshtanko@gmail.com>
2020-08-05 11:57:16 +0300

    Удалил ссылку Skip to content

--- header.php
+++ header.php
@@ -23,7 +23,6 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'konferencii2' ); ?></a>
 
 	<header class="header">
 		<div class="header-top-banner">
