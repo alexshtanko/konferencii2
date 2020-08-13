2ec5ee72172f1e04982e854f293169f59bd44bd3
alex <alexshtanko@gmail.com>
2020-08-13 14:30:33 +0300

    Подкорректировал шаблоны страниц

--- 404.php
+++ 404.php
@@ -10,51 +10,63 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<div class="container">
+	<div class="row">
+		<div class="col-lg-9">
+			<main id="primary" class="site-main" data="404-template">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'konferencii2' ); ?></h1>
-			</header><!-- .page-header -->
+				<section class="error-404 not-found">
+					<header class="page-header">
+						<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'konferencii2' ); ?></h1>
+					</header><!-- .page-header -->
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'konferencii2' ); ?></p>
+					<div class="page-content">
+						<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'konferencii2' ); ?></p>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'konferencii2' ); ?></h2>
-						<ul>
 							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
+							get_search_form();
+
+							the_widget( 'WP_Widget_Recent_Posts' );
 							?>
-						</ul>
-					</div><!-- .widget -->
 
-					<?php
-					/* translators: %1$s: smiley */
-					$konferencii2_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'konferencii2' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$konferencii2_archive_content" );
+							<div class="widget widget_categories">
+								<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'konferencii2' ); ?></h2>
+								<ul>
+									<?php
+									wp_list_categories(
+										array(
+											'orderby'    => 'count',
+											'order'      => 'DESC',
+											'show_count' => 1,
+											'title_li'   => '',
+											'number'     => 10,
+										)
+									);
+									?>
+								</ul>
+							</div><!-- .widget -->
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+							<?php
+							/* translators: %1$s: smiley */
+							$konferencii2_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'konferencii2' ), convert_smilies( ':)' ) ) . '</p>';
+							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$konferencii2_archive_content" );
 
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+							the_widget( 'WP_Widget_Tag_Cloud' );
+							?>
 
-	</main><!-- #main -->
+					</div><!-- .page-content -->
+				</section><!-- .error-404 -->
+
+			</main><!-- #main -->
+		</div>
+
+		<div class="col-lg-3">
+			<?php get_sidebar(); ?>
+		</div>
+
+	</div><!-- row end here -->
+</div><!--container end here-->
 
 <?php
+
 get_footer();
--- single.php
+++ single.php
@@ -10,31 +10,42 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<div class="container">
+	<div class="row">
+		<div class="col-lg-9">
+			<main id="primary" class="site-main" data="single-template">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+				<?php
+				while ( have_posts() ) :
+					the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+					get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'konferencii2' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'konferencii2' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+					the_post_navigation(
+						array(
+							'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'konferencii2' ) . '</span> <span class="nav-title">%title</span>',
+							'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'konferencii2' ) . '</span> <span class="nav-title">%title</span>',
+						)
+					);
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 
-		endwhile; // End of the loop.
-		?>
+				endwhile; // End of the loop.
+				?>
 
-	</main><!-- #main -->
+			</main><!-- #main -->
+		</div>
+
+		<div class="col-lg-3">
+			<?php get_sidebar(); ?>
+		</div>
+
+	</div><!-- row end here -->
+</div><!--container end here-->
 
 <?php
-get_sidebar();
+
 get_footer();
